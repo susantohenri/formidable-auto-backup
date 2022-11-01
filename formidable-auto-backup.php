2b23df1a2b38c5c9b283ab43d7c6d8989632292e
henrisusanto <henri@member.id>
2022-11-01 08:37:52 +0700

    init

---
+++ formidable-auto-backup.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Formidable AutoBackup
+ *
+ * @package     FormidableAutoBackup
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Formidable AutoBackup
+ * Plugin URI:  https://github.com/susantohenri
+ * Description: Formidable Add on to periodically backup everything to XML
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri
+ * Text Domain: Formidable-AutoBackup
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
